21345ca9f2e01ee3e8af27e06ceb17df73d929cc
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-26 23:33:34 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,6 @@
+const  numbers=[1,4,87,8,65,78,12,47,48,24]
+console.log(numbers.length)
+console.log(numbers[1])
+
+numbers[0]=100;
+console.log(numbers)
---
+++ push-pop.js
@@ -0,0 +1,9 @@
+const number=[1,2,3,4,5,6]
+console.log(number)
+number.push(10,20,30)
+
+console.log(number)
+number.pop()
+number.pop()
+
+console.log(number)

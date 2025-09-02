3c8b5cc7e7a9cdca257f37f9cceb7668ab67e554
sohanur24059 <sohanurrahman24059@gmail.com>
2025-09-02 19:41:55 +0600

    send tthis

--- sort.js
+++ sort.js
@@ -1,5 +1,14 @@
 const person=['sohanur','rahman','akib','makib'];
 console.log(person.sort());
 
-const num=[4,5,7,1,2,6];
+const num=[4,5,7,12,26,6];
+// Not working prperly
 console.log(num.sort())
+// Ascending oreder
+console.log(num.sort(function(a,b){ return a-b }))
+
+// descending order
+
+console.log(num.sort(function(a,b){ return b-a }))
+
+ 
